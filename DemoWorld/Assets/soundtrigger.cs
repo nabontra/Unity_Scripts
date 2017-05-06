9b3759733c5ffdad553e11aa103186c617ae3b77
Nick Bontrager <nick@nickbontrager.com>
2017-05-05 19:16:03 -0500

    added sound demos

---
+++ DemoWorld/Assets/soundtrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundtrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        if (!audio.isPlaying)
+            {
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+
+    }
+}
